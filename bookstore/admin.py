956aa74c32456bc5a6ed8064107c556ad11a906d
PhilippMakoviy <philipp_1283@mail.com>
2021-04-06 21:47:24 +0300

    cleaning subscriptions

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Здесь регистрируем созданные модели bookstore и Author
-from .models import Book , Author
+from .models import Book , Author , Subscription
 admin.site.register(Book)
 admin.site.register(Author)
+admin.site.register(Subscription)
